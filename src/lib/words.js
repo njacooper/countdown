e99642c0aac16d5e68134cdeafec3f1e5ca3b0bb
njacooper <neal.cooper@me.com>
2022-09-29 21:24:10 +0100

    Created function for binary sorting a given string into alphabetical order, along with accompanying test.

--- src/lib/words.js
+++ src/lib/words.js
@@ -5,6 +5,25 @@ const swapLetters = (index1, index2, lettersArray) => {
   return lettersArray
 }
 
+const binarySortString = letters => {
+  let lettersArray = letters.split('')
+
+  for (let i = 0; i < lettersArray.length; i++) {
+    for (let j = i + 1; j < lettersArray.length; j++) {
+      if (lettersArray[j] < lettersArray[i]) {
+        swapLetters(i, j, lettersArray)
+      }
+    }
+  }
+
+  //Convert char array back to string
+  let newLetters = lettersArray.join('')
+
+  //Return sorted string
+  return newLetters
+}
+
 module.exports = {
-  swapLetters
+  swapLetters,
+  binarySortString
 }
--- src/lib/words.test.js
+++ src/lib/words.test.js
@@ -1,7 +1,11 @@
-const { swapLetters } = require('./words')
+const { swapLetters, binarySortString } = require('./words')
 
 test('Expect the letters at specified indexes to be swapped with each other', () => {
   expect(
     swapLetters(3, 6, ['t', 'e', 'l', 's', 'v', 'i', 'e', 'i', 'o', 'n'])
   ).toEqual(['t', 'e', 'l', 'e', 'v', 'i', 's', 'i', 'o', 'n'])
+})
+
+test('Expect given string to be returned with letters arranged in alphabetical order', () => {
+  expect(binarySortString('daefcgbh')).toBe('abcdefgh')
 })
