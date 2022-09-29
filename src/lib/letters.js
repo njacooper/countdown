d92fbf1ef79064226c8db41352d1975af9e711f1
njacooper <neal.cooper@me.com>
2022-09-29 20:56:13 +0100

    Created function that returns all consonants and accompanying test.

--- src/lib/letters.js
+++ src/lib/letters.js
@@ -13,4 +13,55 @@ const getAllVowels = () => {
   return vowels
 }
 
-module.exports = { getAllVowels }
+const getAllConsonants = () => {
+  //Countdown Consonants
+  let consonantB = 'bb'
+  let consonantC = 'ccc'
+  let consonantD = 'dddddd'
+  let consonantF = 'ff'
+  let consonantG = 'ggg'
+  let consonantH = 'hh'
+  let consonantJ = 'j'
+  let consonantK = 'k'
+  let consonantL = 'lllll'
+  let consonantM = 'mmmm'
+  let consonantN = 'nnnnnnnn'
+  let consonantP = 'pppp'
+  let consonantQ = 'q'
+  let consonantR = 'rrrrrrrrr'
+  let consonantS = 'sssssssss'
+  let consonantT = 'ttttttttt'
+  let consonantV = 'v'
+  let consonantW = 'w'
+  let consonantX = 'x'
+  let consonantY = 'y'
+  let consonantZ = 'z'
+
+  let consonants =
+    consonantB +
+    consonantC +
+    consonantD +
+    consonantF +
+    consonantG +
+    consonantH +
+    consonantJ +
+    consonantK +
+    consonantL
+  consonants +=
+    consonantM +
+    consonantN +
+    consonantP +
+    consonantQ +
+    consonantR +
+    consonantS +
+    consonantT +
+    consonantV +
+    consonantW
+  consonants += consonantX + consonantX + consonantY + consonantZ
+
+  consonants = consonants.split('')
+
+  return consonants
+}
+
+module.exports = { getAllVowels, getAllConsonants }
