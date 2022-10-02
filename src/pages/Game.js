e0e0fc1178bf8ea374e06e787177cb7f4dc0ae8b
njacooper <neal.cooper@me.com>
2022-10-02 22:37:35 +0100

    Added useEffect that runs every state.letters update and checks if the vowel or consonant buttons should be disabled by counting the number of occurrances of each.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { useReducer } from 'react'
+import { useState, useEffect, useReducer } from 'react'
 
 import GameLettersSelection from '../components/GameLettersSelection'
 
 import { getShuffledConsonants, getShuffledVowels } from '../lib/letters'
 
 function Game () {
-  const initialLetters = []
+  //maximum number of letters in total allowed
+  const [lettersMax, setLettersMax] = useState(9)
+  //keep track of how many letters are remaining to be picked
+  const [lettersRemaining, setLettersRemaining] = useState(9)
 
-  const initialVows = getShuffledVowels()
-  const initialCons = getShuffledConsonants()
+  //if the maximum number of vowels or consonants are picked disable their buttons
+  const [vowelDisabled, setVowelDisabled] = useState(false)
+  const [consonantDisabled, setConsonantDisabled] = useState(false)
+
+  //if the maximum number of letters has been picked, also disable the auto button
+  const [autoDisabled, setAutoDisabled] = useState(false)
 
   const reducer = (state, action) => {
     console.log('state', state)
@@ -25,7 +32,7 @@ function Game () {
         console.log('state vows: ', state.vows)
         console.log('letters: ', state.letters)
         console.log('state cons: ', state.cons)
-        //return the updated 
+        //return the updated
         return {
           ...state,
           vows: state.vows,
@@ -54,9 +61,9 @@ function Game () {
   }
 
   const initialState = {
-    vows: initialVows,
-    cons: initialCons,
-    letters: initialLetters
+    vows: getShuffledVowels(),
+    cons: getShuffledConsonants(),
+    letters: []
   }
 
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -82,32 +89,105 @@ function Game () {
   //dispatch reset
   const handleReset = () => {
     console.log('clicked reset')
-
+    //reset initial states for letters, consonants and vowels
     dispatch({ type: 'RESET' })
+    setVowelDisabled(false)
+    setConsonantDisabled(false)
+  }
+
+  //after every change to the state.letters, check if the vowel or consonant buttons should be disabled
+  useEffect(() => {
+    if (state.letters.length == 9) {
+      setVowelDisabled(true)
+      setConsonantDisabled(true)
+      setAutoDisabled(true)
+    }
+
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let consonants = [
+      'b',
+      'c',
+      'd',
+      'f',
+      'g',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'p',
+      'q',
+      's',
+      't',
+      'v',
+      'x',
+      'z',
+      'h',
+      'r',
+      'w',
+      'y'
+    ]
+
+    let consonantCount = 0
+    for (let i = 0; i < consonants.length; i++) {
+      consonantCount =
+        consonantCount + getOccurrence(state.letters, consonants[i])
+    }
+
+    let vowelCount = 0
+    for (let i = 0; i < vowels.length; i++) {
+      vowelCount = vowelCount + getOccurrence(state.letters, vowels[i])
+    }
+
+    let remainingLetters = state.letters.length - lettersMax
+    //max vowel == max - consonant minimum = 9 - 4 = 5
+    //max cons == max - vowel min = 9 - 3 = 4
+
+    //disable vowel button
+    if ((vowelCount == 5 && lettersRemaining) || lettersRemaining == 0) {
+      setVowelDisabled(true)
+    }
+
+    //disable consonant button
+    if ((consonantCount == 6 && lettersRemaining) || lettersRemaining == 0) {
+      setConsonantDisabled(true)
+    }
+
+    console.log('vowelCount: ', vowelCount)
+    console.log('consonantCount: ', consonantCount)
+  }, [state.letters])
+
+  function getOccurrence (array, value) {
+    var count = 0
+    array.forEach(v => v === value && count++)
+    return count
   }
 
   return (
     <>
       <div className='bg-blue-400'>
         <button
-          className='text-3xl font-bold uppercase text-white p-4'
+          disabled={consonantDisabled}
+          className='text-3xl font-bold uppercase text-white p-4 disabled:bg-gray-400 disabled:cursor-not-allowed disabled:text-opacity-30'
           onClick={handleGetConsonant}
         >
           CONSONANT
         </button>
         <button
-          className='text-3xl font-bold uppercase text-white p-4'
+          disabled={vowelDisabled}
+          className='text-3xl font-bold uppercase text-white p-4 disabled:bg-gray-400 disabled:cursor-not-allowed disabled:text-opacity-30'
           onClick={handleGetVowel}
         >
           VOWEL
         </button>
+
+        <GameLettersSelection letters={state.letters} />
         <button
           className='text-3xl font-bold uppercase text-white p-4'
           onClick={handleReset}
         >
           RESET
         </button>
-        <GameLettersSelection letters={state.letters} />
       </div>
     </>
   )
