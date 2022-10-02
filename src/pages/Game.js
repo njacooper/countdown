b7838668a2d5e2a4304b642751bda3853b4084cb
njacooper <neal.cooper@me.com>
2022-10-02 22:18:00 +0100

    Updated reducer with initial state object containing the initial state for vowels, consonants and letters, along with dispatch actions for retrieving a vowel or consonant and then appending this to the letters state before splicing the retrieved vowel or consonant from their respective states.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -3,39 +3,86 @@ import { useReducer } from 'react'
 
 import GameLettersSelection from '../components/GameLettersSelection'
 
+import { getShuffledConsonants, getShuffledVowels } from '../lib/letters'
+
 function Game () {
   const initialLetters = []
 
+  const initialVows = getShuffledVowels()
+  const initialCons = getShuffledConsonants()
+
   const reducer = (state, action) => {
+    console.log('state', state)
+    console.log('action', action)
     switch (action.type) {
       case 'VOWEL':
-        return [...initialLetters]
+        //Get random to value within range of array
+        let randVow = Math.floor(Math.random() * state.vows.length)
+        //store the consonant in variable
+        let vowel = state.vows[randVow]
+        //remove the vowel
+        state.vows.splice(randVow, 1)
+        console.log('state vows: ', state.vows)
+        console.log('letters: ', state.letters)
+        console.log('state cons: ', state.cons)
+        //return the updated 
+        return {
+          ...state,
+          vows: state.vows,
+          letters: [...state.letters, vowel]
+        }
       case 'CONSONANT':
-        return [...initialLetters]
+        //Get random to value within range of array
+        let randCon = Math.floor(Math.random() * state.vows.length)
+        //store the consonant in variable
+        let consonant = state.cons[randCon]
+        state.cons.splice(randCon, 1)
+        console.log('state vows: ', state.vows)
+        console.log('letters: ', state.letters)
+        console.log('state cons: ', state.cons)
+        return {
+          ...state,
+          cons: state.cons,
+          letters: [...state.letters, consonant]
+        }
+
       case 'RESET':
-        return [...initialLetters]
+        return initialState
       default:
         return state
     }
   }
 
-  const [letters, dispatch] = useReducer(reducer, initialLetters)
-
-  //dispatch vowel
-  const handleGetVowel = letter => {
-    console.log('clicked get vowel')
-    dispatch({ type: 'VOWEL' })
+  const initialState = {
+    vows: initialVows,
+    cons: initialCons,
+    letters: initialLetters
   }
 
-  //dispatch consonant
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  //dispatch get vowel
+  const handleGetVowel = letter => {
+    console.log('clicked get vowel')
+
+    if (state.letters.length != 9) {
+      dispatch({ type: 'VOWEL' })
+    }
+  }
+
+  //dispatch get consonant
   const handleGetConsonant = letter => {
     console.log('clicked get consonant')
-    dispatch({ type: 'CONSONANT' })
+
+    if (state.letters.length != 9) {
+      dispatch({ type: 'CONSONANT' })
+    }
   }
 
   //dispatch reset
   const handleReset = () => {
     console.log('clicked reset')
+
     dispatch({ type: 'RESET' })
   }
 
@@ -60,7 +107,7 @@ function Game () {
         >
           RESET
         </button>
-        <GameLettersSelection letters={letters} />
+        <GameLettersSelection letters={state.letters} />
       </div>
     </>
   )
