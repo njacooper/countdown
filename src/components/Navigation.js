a16f410072aa0ea6d87b07a14882de9194b53a51
njacooper <neal.cooper@me.com>
2022-10-04 19:29:12 +0100

    Added styling to the navigation.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,10 +3,20 @@ import { Routes, Route, Link } from 'react-router-dom'
 function Navigation () {
   return (
     <>
-      <nav>
-        <Link to='/'>Game</Link>
-        <Link to='/manual'>Manual Letters</Link>
-      </nav>
+      <div className='sticky top-0 bg-blue-900 z-50 flex justify-center w-screen py-1 shadow-sm h-16'>
+        <div className='w-full flex justify-center font-bold'>
+          <div className='flex py-4'>
+            <ul className='justify-around flex h-full items-center gap-10 text-white'>
+              <li className='flex'>
+                <Link to='/'>Game</Link>
+              </li>
+              <li className='flex'>
+                <Link to='/manual'>Manual Letters</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
