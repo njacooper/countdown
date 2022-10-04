56e46f4f4a650969c13a131b9a35e035463bbdd7
njacooper <neal.cooper@me.com>
2022-10-04 19:23:50 +0100

    Updated the layout.

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ import ManualLetters from './pages/ManualLetters'
 
 function App () {
   return (
-    <div className='bg-blue-500'>
-      <main>
-        <Routes>
-          <Route path='/' element={<Game />} />
-          <Route path='manual' element={<ManualLetters />} />
-        </Routes>
-
+    <div className='bg-blue-500 min-h-screen'>
+      <main className='bg-blue-400'>
         <Navigation />
+        <div className='mx-auto box-content'>
+          <Routes>
+            <Route path='/' element={<Game />} />
+            <Route path='manual' element={<ManualLetters />} />
+          </Routes>
+        </div>
       </main>
     </div>
   )
