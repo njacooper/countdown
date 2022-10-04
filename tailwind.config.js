5d3f19135fb108c3b2e81af3eb1f87074b6f32d5
njacooper <neal.cooper@me.com>
2022-10-04 15:03:34 +0100

    Added responsive breakpoints for small, medium, large and x-large screens.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
+
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    }
   },
-  plugins: [],
+  plugins: []
 }
