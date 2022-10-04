211ba5d7181bd4c0dafb87b7963f36cb2d68e475
njacooper <neal.cooper@me.com>
2022-10-04 14:41:59 +0100

    Added a style transition and transform for when dragging answer letters.

--- src/components/AnswerItem.js
+++ src/components/AnswerItem.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 const AnswerItem = props => {
   const {
     attributes,
     listeners,
     setNodeRef,
+    transform,
+    transition,
+    isDragging
   } = useSortable({ id: props.id })
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+    border: '4px solid white'
+  }
 
   return (
     <>
-      <div className='bg-blue-500 relative h-20'>
+      <div style={style} className='bg-blue-500 relative h-20'>
         <div className='' ref={setNodeRef}>
           <button
             onClick={() => props.handleRemove(props)}
