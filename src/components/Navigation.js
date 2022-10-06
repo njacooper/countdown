85d6c3f785a35b1a579fd9e358647a1096c7d091
njacooper <neal.cooper@me.com>
2022-10-06 19:37:38 +0100

    Updated navigation styling with active and hover states.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,29 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function Navigation () {
+  //get the path
+  const location = useLocation()
+
   return (
     <>
-      <div className='sticky top-0 bg-blue-900 z-50 flex justify-center w-screen py-1 shadow-sm h-16'>
+      <div className='sticky top-0 bg-[#053259] z-50 flex justify-center w-screen py-1 shadow-sm h-16'>
         <div className='w-full flex justify-center font-bold'>
-          <div className='flex py-4'>
+          <nav className='navigation flex py-4'>
             <ul className='justify-around flex h-full items-center gap-10 text-white'>
-              <li className='flex'>
+              <li className={location.pathname == '/' ? 'activePage' : ''}>
                 <Link to='/'>Game</Link>
+                {location.pathname == '/' && <div className='active'></div>}
               </li>
-              <li className='flex'>
+              <li
+                className={location.pathname == '/manual' ? 'activePage' : ''}
+              >
                 <Link to='/manual'>Manual Letters</Link>
+                {location.pathname == '/manual' && (
+                  <div className='active'></div>
+                )}
               </li>
             </ul>
-          </div>
+          </nav>
         </div>
       </div>
     </>
