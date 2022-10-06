032be4ea3b398422c7886c76085f1972e93b5149
njacooper <neal.cooper@me.com>
2022-10-06 19:31:17 +0100

    Updated styling for the Points component.

--- src/components/Points.js
+++ src/components/Points.js
@@ -28,9 +28,12 @@ function Points (props) {
 
   return (
     <>
-      <p className='w-fit p-4 text-white bg-blue-500 border-4 border-blue-100 border-solid text-5xl font-bold'>
-        {points && points}
-      </p>
+      <div className='flex items-center my-2 border-4 border-solid border-blue-700 bg-blue-800 rounded-xl'>
+        <div className='font-bold text-4xl align-middle px-4 text-white'>Points:</div>
+        <p className='w-fit p-4 text-white text-sm lg:text-5xl font-bold'>
+          {points && points}
+        </p>
+      </div>
     </>
   )
 }
