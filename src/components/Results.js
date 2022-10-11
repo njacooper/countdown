82627581f08aa24024e10df4394d4a0bd8725324
njacooper <neal.cooper@me.com>
2022-10-11 18:18:56 +0100

    Updated heading appearance.

--- src/components/Results.js
+++ src/components/Results.js
@@ -23,103 +23,120 @@ function Results (props) {
 
   return (
     <>
-      <div className=''>
-        <h2 className='font-bold text-2xl py-4'>Found Words:</h2>
-
-        <div className='z-50'>
-          {results.length == 0 ? (
-            <p>No results yet!</p>
-          ) : (
-            <>
-              <div className='p-4 bg-blue-100'>
-                <p>Results of Nine</p>
-                <div className='flex flex-wrap gap-4'>
-                  {nine.map(result => (
-                    <p className='px-3 py-2 bg-blue-300 rounded w-fit'>
-                      {result.word}
-                    </p>
-                  ))}
-                  {nine.length == 0 && <p>No Nines!</p>}
-                </div>
+      <div className='z-50'>
+        {results.length == 0 ? (
+          <h3>No results!</h3>
+        ) : (
+          <>
+            <div className='mb-6'>
+              <h3>Results of Nine</h3>
+              <div className='flex flex-wrap gap-4'>
+                {nine.map(result => (
+                  <p
+                    className='px-3 py-2 bg-blue-300 rounded w-fit'
+                    key={result.word}
+                  >
+                    {result.word}
+                  </p>
+                ))}
+                {nine.length == 0 && <p>No Nines!</p>}
               </div>
+            </div>
 
-              <div className='p-4 bg-blue-100'>
-                <p>Results of Eight</p>
-                <div className='flex flex-wrap gap-4'>
-                  {eight.map(result => (
-                    <p className='px-3 py-2 bg-blue-300 rounded w-fit'>
-                      {result.word}
-                    </p>
-                  ))}
-                  {eight.length == 0 && <p>No Eights!</p>}
-                </div>
+            <div className='mb-6'>
+              <h3>Results of Eight</h3>
+              <div className='flex flex-wrap gap-4'>
+                {eight.map(result => (
+                  <p
+                    className='px-3 py-2 bg-blue-300 rounded w-fit'
+                    key={result.word}
+                  >
+                    {result.word}
+                  </p>
+                ))}
+                {eight.length == 0 && <p>No Eights!</p>}
               </div>
+            </div>
 
-              <div className='p-4 bg-blue-100'>
-                <p>Results of Seven</p>
-                <div className='flex flex-wrap gap-4'>
-                  {seven.map(result => (
-                    <p className='px-3 py-2 bg-blue-300 rounded w-fit'>
-                      {result.word}
-                    </p>
-                  ))}
-                  {seven.length == 0 && <p>No Sevens!</p>}
-                </div>
+            <div className='mb-6'>
+              <h3>Results of Seven</h3>
+              <div className='flex flex-wrap gap-4'>
+                {seven.map(result => (
+                  <p
+                    className='px-3 py-2 bg-blue-300 rounded w-fit'
+                    key={result.word}
+                  >
+                    {result.word}
+                  </p>
+                ))}
+                {seven.length == 0 && <p>No Sevens!</p>}
               </div>
+            </div>
 
-              <div className='p-4 bg-blue-100'>
-                <p>Results of Six</p>
-                <div className='flex flex-wrap gap-4'>
-                  {six.map(result => (
-                    <p className='px-3 py-2 bg-blue-300 rounded w-fit'>
-                      {result.word}
-                    </p>
-                  ))}
-                  {six.length == 0 && <p>No Sixes!</p>}
-                </div>
+            <div className='mb-6'>
+              <h3>Results of Six</h3>
+              <div className='flex flex-wrap gap-4'>
+                {six.map(result => (
+                  <p
+                    className='px-3 py-2 bg-blue-300 rounded w-fit'
+                    key={result.word}
+                  >
+                    {result.word}
+                  </p>
+                ))}
+                {six.length == 0 && <p>No Sixes!</p>}
               </div>
+            </div>
 
-              <div className='p-4 bg-blue-100'>
-                <p>Results of Five</p>
-                <div className='flex flex-wrap gap-4'>
-                  {five.map(result => (
-                    <p className='px-3 py-2 bg-blue-300 rounded w-fit'>
-                      {result.word}
-                    </p>
-                  ))}
+            <div className='mb-6'>
+              <h3>Results of Five</h3>
+              <div className='flex flex-wrap gap-4'>
+                {five.map(result => (
+                  <p
+                    className='px-3 py-2 bg-blue-300 rounded w-fit'
+                    key={result.word}
+                  >
+                    {result.word}
+                  </p>
+                ))}
 
-                  {five.length == 0 && <p>No Fives!</p>}
-                </div>
+                {five.length == 0 && <p>No Fives!</p>}
               </div>
+            </div>
 
-              <div className='p-4 bg-blue-100'>
-                <p>Results of Four</p>
-                <div className='flex flex-wrap gap-4'>
-                  {four.map(result => (
-                    <p className='px-3 py-2 bg-blue-300 rounded w-fit'>
-                      {result.word}
-                    </p>
-                  ))}
+            <div className='mb-6'>
+              <h3>Results of Four</h3>
+              <div className='flex flex-wrap gap-4'>
+                {four.map(result => (
+                  <p
+                    className='px-3 py-2 bg-blue-300 rounded w-fit'
+                    key={result.word}
+                  >
+                    {result.word}
+                  </p>
+                ))}
 
-                  {four.length == 0 && <p>No Fours!</p>}
-                </div>
+                {four.length == 0 && <p>No Fours!</p>}
               </div>
+            </div>
 
-              <div className='p-4 bg-blue-100'>
-                <p>Results of Three</p>
-                <div className='flex flex-wrap gap-4'>
-                  {three.map(result => (
-                    <p className='px-3 py-2 bg-blue-300 rounded w-fit'>
-                      {result.word}
-                    </p>
-                  ))}
+            <div className='mb-6'>
+              <h3>Results of Three</h3>
+              <div className='flex flex-wrap gap-4'>
+                {three.map(result => (
+                  <p
+                    className='px-3 py-2 bg-blue-300 rounded w-fit'
+                    key={result.word}
+                  >
+                    {result.word}
+                  </p>
+                ))}
 
-                  {three.length == 0 && <p>No Threes!</p>}
-                </div>
+                {three.length == 0 && <p>No Threes!</p>}
               </div>
-            </>
-          )}
-        </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   )
